5e843895e9601e6a0155d23a1d135adb5af4b3e2
JeshuranJebanesan <jeshuranj@outlook.com>
2024-08-13 04:39:31 +0100

    sound effects added change format + could clean up code progress to next project

--- assets/js/todo-app.js
+++ assets/js/todo-app.js
@@ -1,7 +1,14 @@
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
+const add_task = document.getElementById("add-task");
+const add = document.getElementById("add");
+const remove = document.getElementById("remove");
+const check = document.getElementById("check");
+
+add_task.addEventListener("click", addTask);
 
 function addTask(){
+    inputBox.value = 'ab';
     if (inputBox.value === ''){
         alert("You must write something!");
     } else {
@@ -11,6 +18,7 @@ function addTask(){
         let span = document.createElement("span");
         span.innerHTML = "\u00d7";
         li.appendChild(span);
+        add.play();
     }
     inputBox.value = ''
     saveData();
@@ -20,9 +28,11 @@ listContainer.addEventListener("click", function(e){
     if(e.target.tagName === "LI"){
         e.target.classList.toggle("checked");
         saveData();
+        check.play();
     } else if(e.target.tagName === "SPAN"){
         e.target.parentElement.remove();
         saveData();
+        remove.play();
     }
 })
 
