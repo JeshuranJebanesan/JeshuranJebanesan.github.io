4dcb4697073da6015dfe7128f2eb75b0bb06c969
JeshuranJebanesan <jeshuranj@outlook.com>
2024-08-13 19:18:13 +0100

    api key city weather

---
+++ assets/js/weatherapp/weatherapp.js
@@ -0,0 +1,11 @@
+const apiKey="6b2ce5cb28e35b71ec9e86bc1f84f317";
+const apiUrl="https://api.openweathermap.org/data/2.5/weather?units=metric&q=germany";
+
+async function checkWeather(){
+    const response = await fetch('${apiUrl}&appid=${apiKey}');
+    var data = response.json();
+
+    console.log(data);
+}
+
+checkWeather();
